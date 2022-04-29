b88049e778300458e70d0bb84881fd21d2e2dd03
Colin <colin.panter.1@ulaval.ca>
2022-04-29 02:40:37 -0400

    Added pages

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 export default function main() {
-  console.log("Loaded")
+    setupNavBar();
+    console.log("Loaded");
+}
+
+function setupNavBar() {
+
+    document.getElementById("to-about-me").addEventListener("click", () => {goToPage(0)})
+    document.getElementById("to-experience").addEventListener("click", () => {goToPage(1)})
+    document.getElementById("to-projects").addEventListener("click", () => {goToPage(2)})
+    document.getElementById("to-contact").addEventListener("click", () => {goToPage(3)})
+}
+
+function goToPage(pageNumber) {
+    const page = document.getElementById("page");
+    const height = page.clientHeight;
+
+    page.scrollTo(0, pageNumber*height)
 }

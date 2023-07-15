a8fc4bbf715b1c29a4a96f4d602ca23d11501cc0
Colin <colin.panter.1@ulaval.ca>
2023-07-15 03:44:56 -0400

    WIP : Experience

--- src/components/MyExperience.js
+++ src/components/MyExperience.js
@@ -1,9 +1,33 @@
 import './MyExperience.css';
 
+import { useState } from 'react';
+
+
+const experiences = [
+  {
+    name: "Test 1",
+    date: "2022-2023",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    name: "Test 2",
+    date: "2022-2024",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }
+]
+
+
 function MyExperience({language}) {
+  const [displayed, setDisplayed] = useState(experiences[0])
+
   return (
     <div className="myexperience">
-        <div></div>
+        <div class="choices">
+        {experiences.map((item,index)=>{
+          return <p className={displayed.name===item.name ? "active" : ""} onClick={()=>{setDisplayed(item)}}>&gt; {item.name}</p>
+        })}
+        </div>
+        <div class="description">{displayed.description}</div>
     </div>
   );
 }

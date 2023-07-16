8edfa7f62d1494b04f642051f210233cfa30b0de
Colin <colin.panter.1@ulaval.ca>
2023-07-16 04:48:53 -0400

    WIP : Projects + QoL stuff

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -4,7 +4,18 @@ function MyProjects({language}) {
   return (
     <div className="myprojects" id='projects'>
         <a style={{"text-decoration": "none"}} href="https://colinpanter.github.io/photoalgo/">
-          <div class="project"><p>Photo Algo</p></div>
+          <div className="project">
+            <p className="project-name">Photo Algo</p>
+            <p className="project-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p className="project-tech">Python - Math</p>
+          </div>
+        </a>
+        <a style={{"text-decoration": "none"}} href="https://colinpanter.github.io/bestagon/">
+          <div className="project">
+            <p className="project-name">Bestagons</p>
+            <p className="project-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p className="project-tech">React - Pathfinding</p>
+          </div>
         </a>
     </div>
   );
